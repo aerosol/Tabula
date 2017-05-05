1d00c5e73ad7b639e1ca3c15bed1fbbad575962c
Adam Rutkowski <hq@mtod.org>
2017-05-06 00:25:56 +0200

    :nail_care:

--- lib/tabula.ex
+++ lib/tabula.ex
@@ -10,6 +10,7 @@ defmodule Tabula do
   ]
 
   @index "#"
+  @newline '\n'
 
   @sheets \
   org_mode: [
@@ -53,77 +54,80 @@ defmodule Tabula do
   end
 
   defimpl Row, for: Map do
-    def get(row, col, default \\ nil), do: row |> Map.get(col, default)
-    def keys(row), do: row |> Map.keys
+    def get(row, col, default \\ nil), do: Map.get(row, col, default)
+    def keys(row), do: Map.keys(row)
   end
 
   defimpl Row, for: List do
-    def get(row, col, default \\ nil), do: row |> Keyword.get(col, default)
-    def keys(row), do: row |> Keyword.keys
+    def get(row, col, default \\ nil), do: Keyword.get(row, col, default)
+    def keys(row), do: Keyword.keys(row)
   end
 
   defimpl Row, for: Any do
-    def get(%{__struct__: _} = row, col, default \\ nil), do: row |> Map.get(col, default)
-    def keys(row), do: row |> Map.from_struct |> Map.keys
+    def get(%{__struct__: _} = row, col, default \\ nil), do: Map.get(row, col, default)
+    def keys(%{__struct__: _} = row) do
+      row
+      |> Map.from_struct
+      |> Map.keys
+    end
   end
 
-  def print_table(rows, opts \\ []) do
-    render_table(rows, opts)
-    |> IO.puts
-  end
+  def print_table(rows, opts \\ []), do: IO.puts render_table(rows, opts)
 
   def render_table(rows, opts \\ []) do
-    cols = extract_cols(rows, opts)
-    _render_table(rows, cols, opts)
+    rows
+    |> extract_cols(opts)
+    |> render_table(rows, opts)
     |> :erlang.list_to_binary
   end
 
-  def max_widths(cols, rows) do
-    max_index = rows |> length |> strlen
-    cols |> map(fn k ->
-      max([
-        strlen(k), max_index | map(rows, &(Row.get(&1, k) |> strlen))
-      ])
-    end)
-  end
-
-  defp _render_table(rows, [_|_]=cols, opts) do
+  defp render_table([_ | _] = cols, rows, opts) do
     widths     = max_widths(cols, rows)
-    formatters = widths |> formatters(opts)
-    spacers    = widths |> spacers(opts)
+    formatters = formatters(widths, opts)
+    spacers    = spacers(widths, opts)
 
-    [ cols    |> render_row(:heading, formatters, opts),
-      spacers |> render_row(:heading_border, formatters, opts),
+    [ render_row(cols, :heading, formatters, opts),
+      render_row(spacers, :heading_border, formatters, opts),
 
       rows
       |> with_index
       |> map(fn indexed_row ->
-              values(cols, indexed_row)
+              cols
+              |> values(indexed_row)
               |> render_row(:row, formatters, opts)
              end) ]
-
   end
 
-  defp extract_cols([h|_], opts) do
+  def max_widths(cols, rows) do
+    max_index =
+      rows
+      |> length
+      |> strlen
+
+    map(cols, fn k ->
+      max([ strlen(k), max_index | map(rows, &strlen(Row.get(&1, k))) ])
+    end)
+  end
+
+  defp extract_cols([first | _], opts) do
     case opts[:only] do
-      nil -> h |> Row.keys
       cols when is_list(cols) -> cols
+      nil                     -> Row.keys(first)
     end
   end
 
   defp render_row(cells, style_element, formatters, opts) do
     separator = style(style_element, opts)
+
     cells
     |> zip(formatters)
     |> map(fn {k, f} -> f.(k) end)
     |> intersperse(separator)
-    |> concat('\n')
+    |> concat(@newline)
   end
 
-  defp render_cell(v) when is_binary(v),
-    do: v
-  defp render_cell(v) when is_number(v),
-    do: inspect(v)
+  defp render_cell(v) when is_binary(v), do: v
+  defp render_cell(v) when is_number(v), do: inspect(v)
   defp render_cell(%{__struct__: _} = v) do
     if String.Chars.impl_for(v) do
       to_string(v)
@@ -131,8 +135,7 @@ defmodule Tabula do
       inspect(v)
     end
   end
-  defp render_cell(v),
-    do: inspect(v)
+  defp render_cell(v), do: inspect(v)
 
   defp formatters(widths, _opts) do
     map(widths, fn w ->
@@ -142,26 +145,33 @@ defmodule Tabula do
          cell when is_binary(cell) ->
            String.ljust(cell, w)
          cell when is_number(cell) ->
-           cell |> render_cell() |> String.rjust(w)
+           cell
+           |> render_cell()
+           |> String.rjust(w)
          cell ->
-           cell |> render_cell() |> String.ljust(w)
+           cell
+           |> render_cell()
+           |> String.ljust(w)
       end
     end)
   end
 
   defp spacers(widths, opts) do
-    map(widths, &(Stream.repeatedly(fn -> style(:spacer, opts) end))
+    map widths, &(Stream.repeatedly(fn -> style(:spacer, opts) end)
                 |> Stream.take(&1)
                 |> join)
   end
 
-  defp strlen(x), do: render_cell(x) |> String.length()
+  defp strlen(x) do
+    x
+    |> render_cell
+    |> String.length
+  end
 
   defp values(cols, {row, index}) do
-    cols
-    |> map(fn (@index) -> index+1
-      (col) -> Row.get(row, col)
-    end)
+    map(cols, fn (@index) -> index + 1
+                 (col)    -> Row.get(row, col)
+              end)
   end
 
   defp style(style, opts) do
